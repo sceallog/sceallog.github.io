750a21e990e47a64db52cf12c17218e807066e6f
Sven Buettner <sven.buettner1@gmail.com>
2020-10-17 19:23:46 +0900

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
 function activateGallery() {
-  alert("Hello from the gallery file!")
+  let thumbnails = document.querySelectorAll("#gallery-thumbs div img")
+  let mainImage = document.querySelector("#gallery-photo img")
+
+  // Image info to be updated.
+  let galleryInfo = document.querySelector("#gallery-info")
+  let title = galleryInfo.querySelector(".title")
+  let description = galleryInfo.querySelector(".description")
+
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", function () {
+      // Set clicked image as display image.
+      let newImageSrc = thumbnail.dataset.largeVersion
+      mainImage.setAttribute("src", newImageSrc)
+      mainImage.setAttribute("alt", thumbnail.alt)
+
+      // Change which image is current.
+      let currentClass = "current"
+      document.querySelector(".current").classList.remove(currentClass)
+      thumbnail.parentNode.classList.add(currentClass)
+
+      // Update image info.
+      title.innerHTML = thumbnail.dataset.title
+      description.innerHTML = thumbnail.dataset.description
+    })
+  })
 }
